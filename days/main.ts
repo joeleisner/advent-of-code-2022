d97aa177da9d83472930ec4044c12d74d1645e1a
Joel Eisner <jeisner93@gmail.com>
2022-12-11 18:33:48 -0500

    Days task modularization
    Adds a new `days/mod.ts` to encapsulate some of the logic/code to run the task for running all days.

--- days/main.ts
+++ days/main.ts
@@ -1,19 +1,5 @@
-const days = [];
+import { days, runDay } from './mod.ts';
 
-for await (
-    const { name, isDirectory } of Deno.readDir(new URL('./', import.meta.url))
-) {
-    if (isDirectory) days.push(name);
-}
-
-import { cyan, green } from 'std/fmt/colors.ts';
-import { string } from '@lib/mod.ts';
-
-for (const day of days.sort()) {
-    const [number, ...title] = day.split('-');
-    console.log(
-        green(`Day ${number}:`),
-        cyan(title.map(string.capitalize).join(' ')),
-    );
-    await import(`./${day}/main.ts`);
+for (const day of days) {
+    await runDay(day);
 }
