26831074964504279e414d93b91f7ed2e186ef97
Joel Eisner <jeisner93@gmail.com>
2022-12-12 15:08:38 -0500

    Day 12: Hill Climbing Algorithm
    Adds the completed code challenge for day 12 (Hill Climbing Algorithm)
    with some changes to the original WIP code. The code can be run via
    `deno task day 12` and tested via `deno test
    days/12-hill-climbing-algorithm`.

--- days/12-hill-climbing-algorithm/mod_test.ts
+++ days/12-hill-climbing-algorithm/mod_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from 'std/testing/asserts.ts';
-import { parseInput } from './mod.ts';
+import { getFewestSteps, parseInput } from './mod.ts';
 
 const input = `Sabqponm
 abcryxxl
@@ -15,15 +15,45 @@ Deno.test('Day 12: Hill Climbing Algorithm', async (test) => {
                 parseInput(input),
                 {
                     map: [
-                        [0, 1, 2, 17, 16, 15, 14, 13],
+                        [1, 1, 2, 17, 16, 15, 14, 13],
                         [1, 2, 3, 18, 25, 24, 24, 12],
-                        [1, 3, 3, 19, 26, 0, 24, 11],
+                        [1, 3, 3, 19, 26, 26, 24, 11],
                         [1, 3, 3, 20, 21, 22, 23, 10],
                         [1, 2, 4, 5, 6, 7, 8, 9],
                     ],
                     start: [0, 0],
                     end: [2, 5],
                 },
+            );
+        },
+    );
+
+    const { map, start, end } = parseInput(input);
+
+    await test.step(
+        'Fewest steps (start to end)',
+        () => {
+            assertEquals(
+                getFewestSteps(
+                    map,
+                    start,
+                    end,
+                ),
+                31,
+            );
+        },
+    );
+
+    await test.step(
+        `Fewest steps (end to lowest)`,
+        () => {
+            assertEquals(
+                getFewestSteps(
+                    map,
+                    end,
+                    1,
+                ),
+                29,
             );
         },
     );
