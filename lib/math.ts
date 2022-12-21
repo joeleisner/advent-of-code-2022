56101cb2eb428e1b3f58333af13e85b23cbf5082
Joel Eisner <jeisner93@gmail.com>
2022-12-21 10:35:47 -0500

    New math library functions
    Adds `subtract`, `subtractAll`, `divide`, and `divideAll` functions to
    the shared `lib/math.ts` library. It also adds tests for these new
    functions.

--- lib/math.ts
+++ lib/math.ts
@@ -4,8 +4,20 @@ export const sum = (first: number, second: number) => first + second;
 // Returns the sum of an array of numbers
 export const sumAll = (numbers: number[]) => numbers.reduce(sum);
 
+// Retruns the subtraction of 2 numbers
+export const subtract = (first: number, second: number) => first - second;
+
+// Returns the subtraction of an array of numbers
+export const subtractAll = (numbers: number[]) => numbers.reduce(subtract);
+
 // Returns the multiplication of 2 numbers
 export const multiply = (first: number, second: number) => first * second;
 
 // Returns the multiplication of an array of numbers
 export const multiplyAll = (numbers: number[]) => numbers.reduce(multiply);
+
+// Returns the division of 2 numbers
+export const divide = (first: number, second: number) => first / second;
+
+// Returns the division of an array of numbers
+export const divideAll = (numbers: number[]) => numbers.reduce(divide);
